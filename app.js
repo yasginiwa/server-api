3e8fe30256397ff62aa118acd23e78d0aab4dd9b
李玉刚 <liyugang@liyugangs-Mac-mini.local>
2020-10-20 17:29:42 +0800

    引入passport-jwt模块

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const bodyParser = require('koa-bodyparser')
 const resextra = require('./modules/resextra')
 const users = require('./routes/api/v1/public/users')
 const { baseURL } = require('./config/default')
+const passport = require('koa-passport')
 
 let app = new Koa()
 
 //  初始化bodyparser
 app.use(bodyParser())
+
+//  初始化koa-passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+//  回调到config文件夹中 passport.js
+require('./config/passport')(passport)
 
 //  统一设置api返回格式
 app.use(resextra)
---
+++ config/passport.js
@@ -0,0 +1,22 @@
+const { jwt_config } = require('../config/default')
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = jwt_config.secretOrKey;
+
+module.exports = passport => {
+    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+        User.findOne({id: jwt_payload.sub}, function(err, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+                // or you could create a new account
+            }
+        });
+    }));
+}
